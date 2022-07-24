bf61fdbe80ceaf0e950674f078b9e0d7943b06c9
CJerkins <fortified.tech.advisor@gmail.com>
2022-07-23 22:51:28 -0400

    add nat

--- after_playbook.sh
+++ after_playbook.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
-sudo apt-get install crowdsec
+sudo apt-get install crowdsec -y
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
-sudo apt install crowdsec-firewall-bouncer-iptables
+sudo apt install crowdsec-firewall-bouncer-iptables -y
 
 curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscale up --authkey <>
